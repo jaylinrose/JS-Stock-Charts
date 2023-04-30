a052fce8ddb4387f527dd7a087b1a215d338b28c
jaylinrose <jaylin.manuel02@gmail.com>
2023-04-29 23:30:27 -0700

    fetch requested stock data and passed 'GME,MSFT,DIS,BNTX' into string

--- public/index.js
+++ public/index.js
@@ -3,7 +3,13 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+   
+    async function timeChartData() {
+        const response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&format=JSON&apikey=1819beb32dad4f549316befaf2e13dff");
+        const jsonData = await response.json();
+        console.log(jsonData);
+      }
+      timeChartData()
 }
 
 main()
